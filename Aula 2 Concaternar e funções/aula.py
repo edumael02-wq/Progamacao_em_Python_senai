081f2c1b39628e3a56ce840be39246eb3a2c5846
edumael02-wq <edumael02@gmail.com>
2025-10-11 12:43:03 -0300

    Add files via upload

---
+++ Aula 2 Concaternar e funções/aula.py
@@ -0,0 +1,83 @@
+# # # sinais aritméticos # + - / // **0,5
+
+# # # sinais 
+
+# # numero1 = 10
+# # n2 = 2
+# # print (numero1 + n2)
+# # print (numero1 - n2)
+# # print (n1 * n2)
+# # print (n1 / n2)
+# # print (n1 // n2)
+# # print (n1 % n2)
+# # print (n1 ** n2)
+
+# # # Sinais logico 
+
+# # n1 = 10
+# # n2 =2
+# # print (n1 > n2)
+# # print (n1 < n2)
+# # print (n1 >= n2)
+# # print (n1 <= n2)
+# # print (n1 != n2)
+# # print (n1 == n2)
+
+# # print() # output --saída
+# # nome = input ('digite seu nome:') # 
+
+# n1 = float(input ('Digite um número: '))
+# n2 = float(input('Digite um número:'))
+# print (n1+n2)
+
+# n1 = float(input ('Digite um número: '))
+# n2 = float(input('Digite um número:'))
+# print (n1-n2)
+
+# # contatnação -- juntar ddos 
+
+# nome = input("Digite seu nome")
+# idade = int(input("Digite sua Idade"))
+# endereco = input('Digite seu endereco')
+# curso = input ('curso')
+# salario = float(input('digite seu salario' ))
+
+# # concaternar 
+# print('nome:', nome)
+# print('sua idade é:', idade )
+# print('endereco:', endereco )
+# print('curso:', curso)
+# # print ('salario:', salario)
+
+n1 = 30
+n2 = 2
+
+print (n1 ** n2)
+
+nome  = input("Digite seu nome:")
+sobrenome = input("Digite seu sobrenome:")
+idade = int(input('Digite sua idade:'))
+n1 = float(input ('Digite um número: '))
+n2 = float(input('Digite um número:'))
+Interiro = ("Números Inteiros:")
+sistema = ('Python')
+Contrato = (1020)
+time = input("Time de coração:")
+time2 = ("Time de coração:")
+
+print(str(nome),str(sobrenome)) 
+print(str(Interiro),str(n1),str(n2)) 
+print(str(sistema),str(Contrato))
+print(str(time2),str(time))
+
+
+
+
+
+
+
+
+
+
+
+
