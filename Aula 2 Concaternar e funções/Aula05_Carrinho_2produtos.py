d1cdc97590b50fc8d15eb97b6814612b32cfe07f
edumael02-wq <edumael02@gmail.com>
2025-10-25 13:03:15 -0300

    Add files via upload

---
+++ Aula 2 Concaternar e funções/Aula05_Carrinho_2produtos.py
@@ -0,0 +1,50 @@
+# inseris
+
+#lista = []
+
+# inserir um dado 
+lista.append(10)
+
+# varios dados de uma vez
+
+lista.extend ([10,30,3010,30,30])
+
+# += - inseri varios dados tb
+
+lista +=(10,30,30,10,30,30)
+
+# insert - inserir na posição que você quiser
+
+lista.insert (0,15000)
+
+print(lista)
+
+# print(lista)
+
+# remover () v
+
+lista.remove(1500)
+
+# del - remove por indice
+
+del lista[0]
+
+# pop() - remove ultimo valor
+lista.pop()
+
+# pop(com idice) - remove o indice que vc quiser
+lista.pop(2)
+print(lista)
+
+#verificar alfo
+# tamonho da lista - len()
+print(len(lista))
+
+# somar a numeros das lista
+
+print(sum(lista))
+
+# max() maior numero da lista
+maior = (lista)
+print (maior)
+
---
+++ Aula 2 Concaternar e funções/Aula05_Carrinho_produtos.py
@@ -0,0 +1,14 @@
+lista = [10,20,30]
+
+lista2 = ['a','b','c']
+
+lista3 = [True,False,True]
+
+lista4 = [6.5,2.5,5]
+
+lista5 = [5.,0,1,False,True]
+
+print (lista[0])
+print (lista[1])
+print (lista[2])
+
