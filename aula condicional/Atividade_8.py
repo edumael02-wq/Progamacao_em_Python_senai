0f0ca888b902f910fafaad7a53f970d8f1bea987
edumael02-wq <edumael02@gmail.com>
2025-11-01 14:53:32 -0300

    Add files via upload

---
+++ aula condicional/Atividade_8.py
@@ -0,0 +1,69 @@
+# numero = int(input ('Digite um número: '))
+
+# if numero > 0:
+#     print("O número é positivo.")
+# elif numero < 0:
+#     print("O número é negativo.")
+# else:
+#     print("O número é zero.")
+
+
+# idade = int(input ('Digite sua idade: '))
+
+# if idade < 16:
+#     print("Você ainda não pode votar.")
+# elif 16 <= idade < 18 or idade >= 70:
+#     print("O voto é opcional.")
+# else:
+#     print("O voto é obrigatório.")
+
+    
+Variavel = int(input ('Digite um número: '))
+
+if Variavel % 2 == 0:
+    print(' número é par.')
+else:
+    print(' número é ímpar.')
+
+
+# n1 = int(input ('Digite um número: '))
+# n2 = int(input ('Digite um número: '))
+# n3 = int(input ('Digite um número: '))
+
+# if (n1 + n2 > n3) and (n1 + n3 > n2) and (n2 + n3 > n1):
+    
+#     if n1 == n2 == n3:
+#         print('triângulo é equilátero.')
+#     elif n1 == n2 or n1 == n3 or n2 == n3:
+#         print('triângulo é isósceles.')
+#     else:
+#         print('triângulo é escaleno.')
+# else:
+#     print("Os valores informados não formam um triângulo.")
+
+
+#     numero = int(input('Digite um número: '))
+
+# if numero % 5 == 0 and numero % 7 == 0:
+#     print('O número é múltiplo de 5 e 7')
+# else:   
+#     print('O número NÃO é múltiplo de 5 e 7')
+
+
+# numero = int(input ('Digite um número: '))
+
+# if numero > 0 and numero > 10:
+#     print('número é positivo e maior que 10')
+# else:
+#     print('número não é positivo e maior que 10')
+
+
+# numero = int(input('Digite um número: '))
+
+# if numero % 3 == 0 or numero % 5 == 0:
+#     print('O número é divisível de 3 e 5')
+# else:   
+#     print('O número NÃO é divisível de 3 e 5')
+
+
+
